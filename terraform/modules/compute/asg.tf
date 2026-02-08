c2ecab87c0b9088328de596974f1427c2ade4619
Shivam <panchbhaishivam@gmail.com>
2026-02-08 19:46:21 +0530

    Update asg.tf

--- terraform/modules/compute/asg.tf
+++ terraform/modules/compute/asg.tf
@@ -31,7 +31,7 @@ resource "aws_autoscaling_group" "docker_asg" {
     version = "$Latest"
   }
 
-  health_check_type         = "EC2"
+  health_check_type         = "ELB"
   health_check_grace_period = 60
 
   instance_refresh {
