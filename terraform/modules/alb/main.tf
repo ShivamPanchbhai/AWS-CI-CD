fc19df7f44185d51b1afac61ea7529eafb4bb281
Shivam <panchbhaishivam@gmail.com>
2026-02-08 19:04:11 +0530

    Update main.tf

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -1,11 +1,41 @@
 ############################################
-# Application Load Balancer
+        # Security Group for ALB
+############################################
+resource "aws_security_group" "alb_sg" {
+  name        = "${var.service_name}-alb-sg"
+  description = "Allow HTTP and HTTPS to ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+############################################
+      # Application Load Balancer
 ############################################
 resource "aws_lb" "this" {
   name               = "${var.service_name}-alb"
   internal           = false
   load_balancer_type = "application"
   subnets            = var.subnet_ids
+  security_groups = [aws_security_group.alb_sg.id]
 
   enable_deletion_protection = false
 }
