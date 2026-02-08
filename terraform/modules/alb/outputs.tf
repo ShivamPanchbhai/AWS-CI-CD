8570cde561263741d1bc6f83bd754a0402cdb30f
Shivam <panchbhaishivam@gmail.com>
2026-02-08 19:39:38 +0530

    Update outputs.tf

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -13,3 +13,9 @@ output "target_group_arn" {
   value = aws_lb_target_group.this.arn
 }
 
+# ALB Security Group ID
+# Used by compute module to allow inbound 8000
+output "alb_security_group_id" {
+  value = aws_security_group.alb_sg.id
+}
+
