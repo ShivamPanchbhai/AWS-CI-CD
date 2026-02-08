6adfe7fc75c3c4eb1b51c32606993a1e3dff41e0
Shivam <panchbhaishivam@gmail.com>
2026-02-08 13:34:24 +0530

    Update outputs.tf

--- terraform/modules/alb/outputs.tf
+++ terraform/modules/alb/outputs.tf
@@ -1,1 +1,15 @@
+############################################
+# ALB outputs
+############################################
+
+# Public DNS name of the load balancer
+output "alb_dns_name" {
+  value = aws_lb.this.dns_name
+}
+
+# Target Group ARN
+# Used by compute (ASG) module
+output "target_group_arn" {
+  value = aws_lb_target_group.this.arn
+}
 
