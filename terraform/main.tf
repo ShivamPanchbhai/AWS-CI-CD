7e16b11cb6ae2fc5142b24e68ba7049f4b717eee
Shivam <panchbhaishivam@gmail.com>
2026-02-08 12:47:15 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,9 @@
+############################################
+# Terraform core configuration
+############################################
 terraform {
 
+  # Define required providers and versions
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -7,25 +11,50 @@ terraform {
     }
   }
 
+  # Remote backend for Terraform state
+  # State is stored centrally in S3
   backend "s3" {
     bucket = "shivam-terraform-state-3069"
     key    = "ec2/terraform.tfstate"
     region = "ap-south-1"
   }
+}
 
-} # terraform block ends
-
+############################################
+# AWS provider configuration
+############################################
 provider "aws" {
+  # All resources will be created in this region
   region = "ap-south-1"
-} # provider block ends
+}
 
-# Get latest Amazon Linux 2023 AMI
-data "aws_ami" "amazon_linux" { # lookup query
-  most_recent = true            # picks the newest one
-  owners      = ["amazon"]
+############################################
+# Data sources
+############################################
 
+# Lookup the latest Amazon Linux 2023 AMI
+# This is used by the Launch Template later
+data "aws_ami" "amazon_linux" {
+
+  # Always fetch the most recent matching AMI
+  most_recent = true
+
+  # Official Amazon-owned AMIs only
+  owners = ["amazon"]
+
+  # Filter to Amazon Linux 2023 x86_64 images
   filter {
     name   = "name"
     values = ["al2023-ami-*-x86_64"]
   }
+}
+
+############################################
+# Modules
+############################################
+
+# ECR module
+# Creates an immutable ECR repository for Docker images
+module "ecr" {
+  source = "./modules/ecr"
 }
