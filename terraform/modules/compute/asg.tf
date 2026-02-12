9941c923a1b4f3b2482d54a8d2ffd087ad01725f
Shivam <panchbhaishivam@gmail.com>
2026-02-13 00:45:20 +0530

    Update asg.tf

--- terraform/modules/compute/asg.tf
+++ terraform/modules/compute/asg.tf
@@ -1,34 +1,51 @@
-# Get default VPC
-data "aws_vpc" "default" {
-  default = true
-}
+########################################################
+# AUTO SCALING GROUP
+# Manages EC2 lifecycle for immutable deployments
+########################################################
 
-# Get all subnets in default VPC
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
+resource "aws_autoscaling_group" "this" {
+  # Name derived from service name for reusability
+  name = "${var.service_name}-asg"
 
-resource "aws_autoscaling_group" "docker_asg" {
-  name = "EC2-ASG"
+  ######################################################
+  # High Availability Configuration
+  ######################################################
 
   min_size         = 2
   max_size         = 2
   desired_capacity = 2
 
+  # Subnets passed from root (do NOT assume default VPC)
   vpc_zone_identifier = var.subnet_ids
 
- target_group_arns = [var.target_group_arn]
+  ######################################################
+  # Attach ALB Target Group
+  ######################################################
+
+  # Target group ARN passed from ALB module
+  target_group_arns = [var.target_group_arn]
+
+  ######################################################
+  # Launch Template Configuration
+  ######################################################
 
   launch_template {
-    id      = aws_launch_template.docker_lt.id
+    id      = aws_launch_template.this.id
     version = "$Latest"
   }
 
+  ######################################################
+  # Health Check Integration
+  # ELB type enables ALB health check feedback
+  ######################################################
+
   health_check_type         = "ELB"
   health_check_grace_period = 60
+
+  ######################################################
+  # Rolling Instance Refresh
+  # Triggered when launch template changes
+  ######################################################
 
   instance_refresh {
     strategy = "Rolling"
@@ -37,12 +54,17 @@ resource "aws_autoscaling_group" "docker_asg" {
       min_healthy_percentage = 50
     }
 
+    # Ensures immutable rollout on LT version change
     triggers = ["launch_template"]
   }
 
+  ######################################################
+  # Resource Tagging
+  ######################################################
+
   tag {
     key                 = "Name"
-    value               = "EC2-ASG"
+    value               = "${var.service_name}-ec2"
     propagate_at_launch = true
   }
 }
