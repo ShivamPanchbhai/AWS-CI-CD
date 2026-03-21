3a74e9c272859e8e1bb5c6f812fbb7f20246a5a1
Shivam Panchbhai <panchbhaishivam@gmail.com>
2026-03-21 10:03:26 +0530

    Update lt.tf

--- terraform/modules/compute/lt.tf
+++ terraform/modules/compute/lt.tf
@@ -5,7 +5,7 @@
 ############################################
 resource "aws_security_group" "ec2_sg" {
   name   = "${var.service_name}-ec2-sg"
-  vpc_id = var.vpc_id
+  vpc_id = data.aws_vpc.default.id
 
   # ALB → App
   ingress {
